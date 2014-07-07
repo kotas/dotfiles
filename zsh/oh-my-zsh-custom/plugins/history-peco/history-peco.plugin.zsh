abffefd1a9b5447725ed6e23b7278c77fff25f47
kotas <kotas@kotas.jp>
2014-07-07 21:30:02 +0900

    Fix peco-history to remove duplicate histories

--- zsh/oh-my-zsh-custom/plugins/history-peco/history-peco.plugin.zsh
+++ zsh/oh-my-zsh-custom/plugins/history-peco/history-peco.plugin.zsh
@@ -1,13 +1,10 @@
 function peco-select-history() {
-    local tac
-    if which tac > /dev/null; then
-        tac="tac"
-    else
-        tac="tail -r"
-    fi
-    BUFFER=$(\history -n 1 | \
-        eval $tac | \
-        peco --query "$LBUFFER")
+    BUFFER=$(
+        \history -n 1 | \
+        tail -r | \
+        ruby -rset -e 's = Set.new; STDIN.each_line { |l| s.add?(l.strip) && puts(l) }' | \
+        peco --query "$LBUFFER"
+    )
     CURSOR=$#BUFFER
     zle clear-screen
 }
